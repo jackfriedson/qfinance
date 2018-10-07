7deb529afeb3e72691d33afff6da485e28c3be5c
Jack <jack.friedson@gmail.com>
2018-10-06 20:58:39 -0400

    Use % return as reward instead of $ return

--- qfinance/environment/core.py
+++ qfinance/environment/core.py
@@ -98,7 +98,7 @@ class Environment(object):
         # TODO: add risk penalty (and maybe market impact penalty)
         new_value = self.portfolio_value
         self._episode_values.iloc[self._state_idx] = new_value
-        return new_value - old_value
+        return (new_value / old_value) - 1.
 
     def plot(self,
              data_column: str = 'close',
@@ -125,9 +125,9 @@ class Environment(object):
             market_data = self._data[slice_][data_column]
             portfolio_data = self._episode_values.iloc[slice_]
             price_data = market_data.join(portfolio_data)
+            assert portfolio_data[0] == self.initial_funding, portfolio_data[0]
 
             scaled_data = price_data / price_data.iloc[0]
-            assert self._episode_values.iloc[self._episode_start] == self.initial_funding
             scaled_data *= self.initial_funding
             dt_index = scaled_data.index
             scaled_data.reset_index(drop=True, inplace=True)
